d5f7e713a57c78578f978241fa1bd85dcb7562da
Hamza Haddani <hhamza@student.1337.ma>
2022-07-17 15:04:50 +0100

    fix(cpp-01/ex04): Fix segfault
    Fix segfault on invalid number of arguments

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 11:19:33 by hhamza            #+#    #+#             */
-/*   Updated: 2022/07/17 13:34:46 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/07/17 15:04:36 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 int	main(int argc, char **argv)
 {
-	SedIsForLosers	sed_is_for_losers(argv[1]);
-
 	if (argc != 4)
 	{
 		std::cerr << "Usage: ./sed_is_for_losers <filename> <s1> <s2>";
 		std::cerr << std::endl;
 		return (1);
 	}
+
+	SedIsForLosers	sed_is_for_losers(argv[1]);
+
 	sed_is_for_losers.run(argv[2], argv[3]);
 	return (0);
 }
