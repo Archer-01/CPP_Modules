11ea50d349b453625121dfe9a16cf53d5e0bef09
Hamza Haddani <hhamza@student.1337.ma>
2022-07-23 21:18:24 +0100

    feat(cpp-04/ex03): Add IMateriaSource interface

---
+++ module_04/ex03/IMateriaSource.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 21:16:12 by hhamza            #+#    #+#             */
+/*   Updated: 2022/07/23 21:17:48 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+# include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+		virtual ~IMateriaSource(void);
+		virtual void		learnMateria(AMateria *m) = 0;
+		virtual AMateria	*createMateria(std::string const &type) = 0;
+};
+
+#endif
