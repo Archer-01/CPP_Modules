61b1efc15858d74775387c33412e644d4e813fdb
Hamza Haddani <hhamza@student.1337.ma>
2022-07-23 18:13:53 +0100

    feat(cpp-04/ex03): Add ICharacter interface

---
+++ module_04/ex03/ICharacter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 18:12:47 by hhamza            #+#    #+#             */
+/*   Updated: 2022/07/23 18:12:58 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include <iostream>
+# include <string>
+
+class AMateria;
+
+class ICharacter
+{
+	public:
+		virtual ~ICharacter(void);
+		virtual std::string const	&getName(void) const = 0;
+		virtual void				equip(AMateria *m) = 0;
+		virtual void				unequip(int idx) = 0;
+		virtual void				use(int idx, ICharacter &target) = 0;
+};
+
+#endif
