963fc25a56bf7b35c9e72696a5ab8d546c30fda9
Hamza Haddani <hhamza@student.1337.ma>
2022-07-25 10:52:57 +0100

    feat(cpp-05/ex02): Add Form class

---
+++ module_05/ex02/Form.cpp
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 21:22:09 by hhamza            #+#    #+#             */
+/*   Updated: 2022/07/24 21:37:50by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::GradeTooHighException::GradeTooHighException(void) :
+	std::runtime_error("Grade too high")
+{
+	// Empty body
+}
+
+Form::GradeTooLowException::GradeTooLowException(void) :
+	std::runtime_error("Grade too low")
+{
+	// Empty body
+}
+
+Form::Form(void) :
+	_name("Form"),
+	_isSigned(false),
+	_gradeToSign(150),
+	_gradeToExecute(150)
+{
+	// Empty body
+}
+
+Form::Form(Form const &src) :
+	_name(src._name),
+	_isSigned(src._isSigned),
+	_gradeToSign(src._gradeToSign),
+	_gradeToExecute(src._gradeToExecute)
+{
+	// Empty body
+}
+
+Form::Form(std::string const &name, short const gradeToSign, \
+	short const getGradeToExecute) :
+	_name(name),
+	_isSigned(false),
+	_gradeToSign(gradeToSign),
+	_gradeToExecute(getGradeToExecute)
+{
+	if (gradeToSign < 1)
+	{
+		throw Form::GradeTooHighException();
+	}
+	else if (gradeToSign > 150)
+	{
+		throw Form::GradeTooLowException();
+	}
+	if (getGradeToExecute < 1)
+	{
+		throw Form::GradeTooHighException();
+	}
+	else if (getGradeToExecute > 150)
+	{
+		throw Form::GradeTooLowException();
+	}
+}
+
+Form::~Form(void)
+{
+	// Empty body
+}
+
+Form	&Form::operator=(Form const &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_isSigned = rhs._isSigned;
+	}
+	return (*this);
+}
+
+std::string const	&Form::getName(void) const
+{
+	return (this->_name);
+}
+
+bool	Form::getIsSigned(void) const
+{
+	return (this->_isSigned);
+}
+
+short	Form::getGradeToSign(void) const
+{
+	return (this->_gradeToSign);
+}
+
+short	Form::getGradeToExecute(void) const
+{
+	return (this->_gradeToExecute);
+}
+
+void	Form::beSigned(Bureaucrat const &bureaucrat)
+{
+	if (bureaucrat.getGrade() > this->_gradeToSign)
+	{
+		throw Form::GradeTooLowException();
+	}
+	this->_isSigned = true;
+	return ;
+}
+
+std::ostream	&operator<<(std::ostream &out, Form const &rhs)
+{
+	out << rhs.getName() << "[";
+	if (rhs.getIsSigned() == true)
+	{
+		out << "Signed";
+	}
+	else
+	{
+		out << "Not signed";
+	}
+	out << "] - Grade to sign: " << rhs.getGradeToSign();
+	out << " / Grade to execute: " << rhs.getGradeToExecute();
+	return (out);
+}
---
+++ module_05/ex02/Form.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 20:26:32 by hhamza            #+#    #+#             */
+/*   Updated: 2022/07/25 10:51:55 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <string>
+# include <stdexcept>
+# include <iostream>
+
+class Bureaucrat;
+# include "Bureaucrat.hpp"
+
+class Form
+{
+	private:
+		class GradeTooHighException : public std::runtime_error
+		{
+			public:
+				GradeTooHighException(void);
+		};
+
+		class GradeTooLowException : public std::runtime_error
+		{
+			public:
+				GradeTooLowException(void);
+		};
+
+		std::string const	_name;
+		bool				_isSigned;
+		short const			_gradeToSign;
+		short const			_gradeToExecute;
+
+		Form(void);
+
+	public:
+		Form(Form const &src);
+		Form(std::string const &name, short const gradeToSign, \
+			short const gradeToExecute);
+		~Form(void);
+		Form				&operator=(Form const &rhs);
+		std::string const	&getName(void) const;
+		bool				getIsSigned(void) const;
+		short				getGradeToSign(void) const;
+		short				getGradeToExecute(void) const;
+		void				beSigned(Bureaucrat const &bureaucrat);
+		virtual void		execute(Bureaucrat const &executor) const = 0;
+};
+
+std::ostream	&operator<<(std::ostream &out, Form const &rhs);
+
+#endif
