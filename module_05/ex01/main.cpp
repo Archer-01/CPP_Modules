d0ead4f0979001cf96ba3d6495e2456c1f8c90d5
Hamza Haddani <hhamza@student.1337.ma>
2022-07-25 07:14:23 +0100

    fix(cpp-05/ex01): Update main tests

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,28 +6,27 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 20:10:40 by hhamza            #+#    #+#             */
-/*   Updated: 2022/07/24 20:19:03 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/07/25 07:05:46 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 #include <iostream>
 
 int	main(void)
 {
+	Form		myForm("myForm", 42, 37);
+	Bureaucrat	me("me", 44);
+
 	try
 	{
-		Bureaucrat	me("me", 1);
-
-		me.decrementGrade();
-		std::cout << me << std::endl;
-		me.incrementGrade();
+		myForm.beSigned(me);
 	}
 	catch (std::exception const &exception)
 	{
 		std::cout << "Error: " << exception.what() << std::endl;
-		return (1);
 	}
-	std::cout << "Success" << std::endl;
+	me.signForm(myForm);
 	return (0);
 }
