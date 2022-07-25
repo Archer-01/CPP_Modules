ae4a5fe3c6f8627c75e0ca1d746f64398ecc65b1
Hamza Haddani <hhamza@student.1337.ma>
2022-07-25 13:08:02 +0100

    feat(cpp-05/ex02): Add tests

--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -6,14 +6,74 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 20:10:40 by hhamza            #+#    #+#             */
-/*   Updated: 2022/07/25 10:50:51 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/07/25 13:06:37 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
+// * ShrubberyCreationForm Test
 int	main(void)
 {
-	std::cout << "Tests are loading..." << std::endl;
+	Bureaucrat				me("Me", 145);
+	ShrubberyCreationForm	shrub("Shrub");
+
+	try
+	{
+		shrub.beSigned(me);
+		me.signForm(shrub);
+		shrub.execute(me);
+	}
+	catch (std::exception const &exception)
+	{
+		std::cerr << "Error: " << exception.what() << std::endl;
+		return (1);
+	}
+	std::cout << "Success!" << std::endl;
 	return (0);
 }
+
+// * RobotomyRequestForm Test
+// int	main(void)
+// {
+// 	Bureaucrat			me("me", 72);
+// 	RobotomyRequestForm	robotomyRequestForm("robotomy");
+
+// 	try
+// 	{
+// 		robotomyRequestForm.beSigned(me);
+// 		me.signForm(robotomyRequestForm);
+// 		robotomyRequestForm.execute(me);
+// 	}
+// 	catch(std::exception const &exception)
+// 	{
+// 		std::cerr << "Error: " << exception.what() << std::endl;
+// 		return (1);
+// 	}
+// 	std::cout << "Success!" << std::endl;
+// 	return (0);
+// }
+
+// * PresidentialPardonForm Test
+// int	main(void)
+// {
+// 	Bureaucrat				me("me", 25);
+// 	PresidentialPardonForm	form("presidential_pardon_target");
+
+// 	try
+// 	{
+// 		form.beSigned(me);
+// 		me.signForm(form);
+// 		form.execute(me);
+// 	}
+// 	catch(std::exception const &exception)
+// 	{
+// 		std::cerr << "Error: " << exception.what() << std::endl;
+// 		return (1);
+// 	}
+// 	return (0);
+// }
