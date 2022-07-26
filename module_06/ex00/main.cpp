d91e0b5d634d439c00c6d64eff3ceb7d47c5a440
Hamza Haddani <hhamza@student.1337.ma>
2022-07-26 20:21:24 +0100

    fix(cpp-06/ex00): Add exception handling

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -6,12 +6,45 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 13:06:53 by hhamza            #+#    #+#             */
-/*   Updated: 2022/07/26 19:55:02 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/07/26 20:21:04 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdexcept>
 #include "Convert.hpp"
+
+static void	convert(std::string const &literal)
+{
+	switch(Convert::getType(literal))
+	{
+		case (Convert::CHAR_TYPE):
+		{
+			Convert::convertChar(literal);
+			break;
+		}
+		case (Convert::FLOAT_TYPE):
+		{
+			Convert::convertFloat(literal);
+			break;
+		}
+		case (Convert::INT_TYPE):
+		{
+			Convert::convertInt(literal);
+			break;
+		}
+		case (Convert::DOUBLE_TYPE):
+		{
+			Convert::convertDouble(literal);
+			break;
+		}
+		default:
+		{
+			std::cout << "UNKNOWN" << std::endl;
+			break;
+		}
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,33 +53,14 @@ int	main(int argc, char **argv)
 		std::cerr << "Usage: ./convert <char|int|float|double>" << std::endl;
 		return (1);
 	}
-	switch(Convert::getType(argv[1]))
+	try
 	{
-		case (Convert::CHAR_TYPE):
-		{
-			Convert::convertChar(argv[1]);
-			break;
-		}
-		case (Convert::FLOAT_TYPE):
-		{
-			Convert::convertFloat(argv[1]);
-			break;
-		}
-		case (Convert::INT_TYPE):
-		{
-			Convert::convertInt(argv[1]);
-			break;
-		}
-		case (Convert::DOUBLE_TYPE):
-		{
-			Convert::convertDouble(argv[1]);
-			break;
-		}
-		default:
-		{
-			std::cout << "UNKNOWN" << std::endl;
-			break;
-		}
+		convert(argv[1]);
+	}
+	catch (std::exception const &exception)
+	{
+		std::cerr << "Error: " << exception.what() << std::endl;
+		return (1);
 	}
 	return (0);
 }
